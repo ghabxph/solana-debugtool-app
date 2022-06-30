89eade0c17da383cc2e4216a90571636aa5c56c0
Marc Par <marc.par10@gmail.com>
2022-06-30 21:01:07 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { PublicKey } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+
+const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID: PublicKey = new PublicKey(
+  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+);
+
+async function findAssociatedTokenAddress(
+    walletAddress: PublicKey,
+    tokenMintAddress: PublicKey
+): Promise<PublicKey> {
+    return (await PublicKey.findProgramAddress(
+        [
+            walletAddress.toBuffer(),
+            TOKEN_PROGRAM_ID.toBuffer(),
+            tokenMintAddress.toBuffer(),
+        ],
+        SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+    ))[0];
+}
+
+ findAssociatedTokenAddress(new PublicKey("") ,new PublicKey("")).then(
+    (value) =>{
+        console.info(value);
+    }
+ )
+
